83e35058e1954884f242b02a9ff08bc58ac84b26
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-02 21:04:38 -0300

    update

--- src/Componnets/Header.jsx
+++ src/Componnets/Header.jsx
@@ -1,20 +1,14 @@
+import { ButtonNav } from "./ButtonNav";
+
 export function Header() {
     return (
         <header className="flex justify-between py-8 px-4 bg-fondo">
             <h1 className="text-aqua font-bold text-3xl">Manuel Carreras</h1>
             <ul className="flex gap-4 text-naranja font-bold text-xl">
-                <li>
-                    <button>Sobre mí</button>
-                </li>
-                <li>
-                    <button>Proyectos</button>
-                </li>
-                <li>
-                    <button>Contacto</button>
-                </li>
-                <li>
-                    <button>Idioma</button>
-                </li>
+                <ButtonNav text="Sobre mí" />
+                <ButtonNav text="Proyectos" />
+                <ButtonNav text="Contacto" />
+                <ButtonNav text="Idioma" />
             </ul>
         </header>
     );
