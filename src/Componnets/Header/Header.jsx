eab426f7149dadb0ad11f65636063141979e4485
ManuGolem <manuelcarreras2013@gmai.com>
2025-03-05 09:57:15 -0300

    add some classes

--- src/Componnets/Header/Header.jsx
+++ src/Componnets/Header/Header.jsx
@@ -2,13 +2,13 @@ import { ButtonNav } from "./ButtonNav";
 
 export function Header() {
     return (
-        <header className="flex justify-between py-8 px-4 bg-fondo">
+        <header className="top-0 left-0 flex justify-between py-8 px-4 bg-fondo fixed w-screen backdrop-blur-2xl">
             <h1 className="text-aqua font-bold text-3xl">Manuel Carreras</h1>
             <nav>
                 <ul className="flex gap-4 text-aqua font-bold text-xl">
-                    <ButtonNav text="Sobre mí" />
-                    <ButtonNav text="Proyectos" />
-                    <ButtonNav text="Contacto" />
+                    <ButtonNav text="Sobre mí" link="#sobremi" />
+                    <ButtonNav text="Proyectos" link="#proyectos" />
+                    <ButtonNav text="Contacto" link="#contacto" />
                     <ButtonNav text="Idioma" />
                 </ul>
             </nav>
