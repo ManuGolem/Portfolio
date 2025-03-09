0124cc4303ff0d4c0052faffe36dc3b0b379337a
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-09 15:04:45 -0300

    add some class

--- src/Componnets/Main/Main.jsx
+++ src/Componnets/Main/Main.jsx
@@ -1,11 +1,15 @@
+import { Contacto } from "./Sections/Contacto";
 import { Proyectos } from "./Sections/Proyectos";
+import { Skills } from "./Sections/Skills";
 import { SobreMi } from "./Sections/SobreMi";
 
 export function Main() {
     return (
-        <main className="flex flex-col gap-5 ">
+        <main className="flex flex-col">
             <SobreMi />
             <Proyectos />
+            <Skills />
+            <Contacto />
         </main>
     );
 }
