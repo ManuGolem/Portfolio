1f1df17f76cb8997abf428decdc3c16557d47d27
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-10 17:13:07 -0300

    agrego estilos varios para la parte de proyectos

--- src/Componnets/Main/Sections/Proyecto.jsx
+++ src/Componnets/Main/Sections/Proyecto.jsx
@@ -2,19 +2,23 @@ export function Proyecto({ title, img, descriptionEs, descriptionEn, link }) {
     function openLink() {
         window.open(link, "_blank");
     }
+
     return (
         <article
-            className=" p-5 hover:rounded-[8px] hover:cursor-pointer hover:text-naranja transition-all ease-in-out hover:scale-[1.02]"
+            className=" p-5 group transition-all ease-in-out hover:scale-[1.02] hover:cursor-pointer"
             onClick={openLink}
         >
-            <h1 className="text-center text-2xl text-aqua font-bold">
+            <h1 className="text-center text-2xl text-aqua-suave font-bold group-hover:text-aqua">
                 {title}
             </h1>
+            <hr className="hidden group-hover:block h-[4px] bg-naranja text-naranja rounded-2xl w-32 mx-auto"></hr>
             <img
-                className="rounded-[8px] my-5 border-aqua border-[4px]"
+                className="rounded-2xl my-5 border-aqua-suave border-[6px] group-hover:border-aqua"
                 src={img}
             />
-            <p>{descriptionEs}</p>
+            <p className="px-8 text-xl group-hover:text-naranja">
+                {descriptionEs}
+            </p>
         </article>
     );
 }
--- src/Componnets/Main/Sections/Proyectos.jsx
+++ src/Componnets/Main/Sections/Proyectos.jsx
@@ -3,9 +3,11 @@ import { Proyecto } from "./Proyecto";
 export function Proyectos() {
     return (
         <section id="proyectos" className="bg-proyecto">
-            <h1 className="text-center my-5 text-3xl font-bold">
+            <h1 className="text-center mt-5 text-3xl font-bold text-aqua">
                 Proyectos Personales
             </h1>
+            <hr className="h-[4px] bg-naranja text-naranja rounded-2xl w-96 mx-auto"></hr>
+
             <div className="grid grid-cols-2 justify-items-center gap-10 p-10">
                 <Proyecto
                     title="Rest Countries Api"
