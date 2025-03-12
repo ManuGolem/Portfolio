ea8dbeb47ff488b383ffc644399bd727bb531cbf
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-12 14:41:45 -0300

    remove

--- src/Componnets/Main/Sections/Proyecto.jsx
+++ src/Componnets/Main/Sections/Proyecto.jsx
@@ -1,25 +1,23 @@
-export function Proyecto({ title, img, descriptionEs, descriptionEn, link }) {
-    function openLink() {
-        window.open(link, "_blank");
-    }
+export function Proyecto({ title, img, description, link }) {
+  function openLink() {
+    window.open(link, "_blank");
+  }
 
-    return (
-        <article
-            className=" p-5 group transition-all ease-in-out hover:scale-[1.02] hover:cursor-pointer"
-            onClick={openLink}
-        >
-            <h1 className="text-center text-2xl text-aqua-suave font-bold group-hover:text-aqua">
-                {title}
-            </h1>
-            <hr className="hidden group-hover:block h-[4px] bg-naranja text-naranja rounded-2xl w-32 mx-auto"></hr>
-            <img
-                className="rounded-2xl my-5 border-aqua-suave border-[6px] group-hover:border-aqua"
-                src={img}
-                alt={`screenshot of the proyect ${title}`}
-            />
-            <p className="px-8 text-xl group-hover:text-naranja">
-                {descriptionEs}
-            </p>
-        </article>
-    );
+  return (
+    <article
+      className=" p-5 group transition-all ease-in-out hover:scale-[1.02] hover:cursor-pointer"
+      onClick={openLink}
+    >
+      <h1 className="text-center text-2xl text-aqua-suave font-bold group-hover:text-aqua">
+        {title}
+      </h1>
+      <hr className="hidden group-hover:block h-[4px] bg-naranja text-naranja rounded-2xl w-32 mx-auto"></hr>
+      <img
+        className="rounded-2xl my-5 border-aqua-suave border-[6px] group-hover:border-aqua"
+        src={img}
+        alt={`screenshot of the proyect ${title}`}
+      />
+      <p className="px-8 text-xl group-hover:text-naranja">{description}</p>
+    </article>
+  );
 }
