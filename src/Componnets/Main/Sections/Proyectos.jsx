c34dcbc21c8faa47b0f9a220ce13b76f9611ec11
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-12 14:42:03 -0300

    remove line

--- src/Componnets/Main/Sections/Proyectos.jsx
+++ src/Componnets/Main/Sections/Proyectos.jsx
@@ -12,29 +12,25 @@ export function Proyectos() {
                 <Proyecto
                     title="Rest Countries Api"
                     img="./img/rest.png"
-                    descriptionEs="Página que muestra información de todos los países, hecha con HTML, JS y CSS. Es mi solución al challenge de Frontend Mentor."
-                    descriptionEn="Page that displays information about all countries, made with HTML, JS, and CSS. It is my solution to the Frontend Mentor challenge."
+                    description="Página que muestra información de todos los países, hecha con HTML, JS y CSS. Es mi solución al challenge de Frontend Mentor."
                     link="https://manugolem.github.io/rest-countries-api-with-color-theme-switcher/"
                 />
                 <Proyecto
                     title="Interactive Comments"
                     img="./img/interactive-comments.png"
-                    descriptionEs="Aplicación de comentarios interactivos hecha en React y TailwindCSS. Se pueden responder, borrar, editar y crear comentarios. Es mi solución al challenge de Frontend Mentor."
-                    descriptionEn="Interactive comment app built with React and TailwindCSS. Users can reply, delete, edit, and create comments. It's my solution to the Frontend Mentor challenge."
+                    description="Aplicación de comentarios interactivos hecha en React y TailwindCSS. Se pueden responder, borrar, editar y crear comentarios. Es mi solución al challenge de Frontend Mentor."
                     link="https://manugolem.github.io/interactive-comments/"
                 />
                 <Proyecto
                     title="Frontend Mentor Solutions"
                     img="./img/frontend.png"
-                    descriptionEs="Página sencilla diseñada para mostrar mis soluciones a los desafíos de Frontend Mentor, hecho con CSS y HTML."
-                    descriptionEn="A simple page designed to showcase my solutions to Frontend Mentor challenges, made with CSS and HTML."
+                    description="Página sencilla diseñada para mostrar mis soluciones a los desafíos de Frontend Mentor, hecho con CSS y HTML."
                     link="https://manugolem.github.io/frontend-mentor-solutions/"
                 />
                 <Proyecto
                     title="To do web"
                     img="./img/todoReact.png"
-                    descriptionEs="Página de lista de tareas (To-Do) desarrollada con React y Tailwind CSS."
-                    descriptionEn="To-Do list page developed with React and Tailwind CSS."
+                    description="Página de lista de tareas (To-Do) desarrollada con React y Tailwind CSS."
                     link="https://manugolem.github.io/ToDoWebReact/"
                 />
             </div>
