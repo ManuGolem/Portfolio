5a674c82bf553db3ddd086fa9d618cbdaf03467c
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-12 21:16:05 -0300

    no hago mucho aca

--- src/Componnets/Main/Main.jsx
+++ src/Componnets/Main/Main.jsx
@@ -2,12 +2,11 @@ import { Contacto } from "./Sections/Contacto";
 import { Proyectos } from "./Sections/Proyectos";
 import { Skills } from "./Sections/Skills";
 import { SobreMi } from "./Sections/SobreMi";
-
-export function Main() {
+export function Main({ textMain }) {
     return (
         <main className="flex flex-col">
-            <SobreMi />
-            <Proyectos />
+            <SobreMi textResumen={textMain.resumen} />
+            <Proyectos textProyectos={textMain.proyectos} />
             <Skills />
             <Contacto />
         </main>
