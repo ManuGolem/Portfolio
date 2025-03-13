703b5b4e7af41a0ac6e659dd906c9c19607f1691
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-12 21:15:30 -0300

    paso los elementos del json a cada componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Header } from "./Componnets/Header/Header";
 import { Main } from "./Componnets/Main/Main";
 import { Footer } from "./Componnets/Footer";
+import { useState } from "react";
+import data from "./text.json";
 export function App() {
+    const [es, setEs] = useState(true);
+    function handleIdioma() {
+        setEs((prev) => !prev);
+    }
+
     return (
         <>
-            <Header />
-            <Main />
+            <Header
+                textButtons={es ? data.es.buttonNavs : data.en.buttonNavs}
+                idioma={es}
+                funcion={handleIdioma}
+            />
+            <Main textMain={es ? data.es.main : data.en.main} />
             <Footer />
         </>
     );
