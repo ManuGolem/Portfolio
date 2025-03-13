38e42831a99d9405cd3b89aca274626abdcfa584
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-12 21:13:05 -0300

    add dinamic render

--- src/Componnets/Header/Header.jsx
+++ src/Componnets/Header/Header.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import { ButtonNav } from "./ButtonNav";
 
-export function Header() {
+export function Header({ textButtons, idioma, funcion }) {
     return (
         <header className="top-0 left-0 flex justify-between py-8 px-8 bg-fondo-header fixed w-screen backdrop-blur-[5px] z-10">
             <h1 className="text-aqua font-bold text-3xl">Manuel Carreras</h1>
             <nav>
                 <ul className="flex gap-4 text-aqua font-bold text-xl">
-                    <ButtonNav text="Sobre mí" link="#sobremi" />
-                    <ButtonNav text="Proyectos" link="#proyectos" />
-                    <ButtonNav text="Habilidades" link="#skills" />
-                    <ButtonNav text="Contacto" link="#contacto" />
-                    <ButtonNav text="Idioma" />
+                    <ButtonNav text={textButtons.sobremi} link="#sobremi" />
+                    <ButtonNav text={textButtons.proyectos} link="#proyectos" />
+                    <ButtonNav text={textButtons.skills} link="#skills" />
+                    <ButtonNav text={textButtons.contacto} link="#contacto" />
+                    <ButtonNav
+                        text={idioma ? "English" : "Spanish"}
+                        funcion={funcion}
+                    />
                 </ul>
             </nav>
         </header>
