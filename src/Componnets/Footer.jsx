ca0ec79277e3c08617e521dc66e194979afd807f
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-14 15:12:39 -0300

    add footer

--- src/Componnets/Footer.jsx
+++ src/Componnets/Footer.jsx
@@ -1,3 +1,20 @@
 export function Footer() {
-    return <footer className="bg-fondo">Esto es el footer</footer>;
+    function rederigir() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    }
+    return (
+        <footer className="text-center bg-fondo py-5">
+            <h1
+                className="text-naranja hover:scale-[1.1] hover:cursor-pointer"
+                onClick={rederigir}
+            >
+                <span className="text-aqua">&lt;/ </span>
+                Manuel Carreras
+                <span className="text-aqua"> &gt;</span>
+            </h1>
+        </footer>
+    );
 }
