d5f1c1122950070856bca596a40bbe86d15aef28
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-15 22:03:48 -0300

    changes configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react(), tailwindcss()],
-});
+    build: {
+        outDir: "docs",
+    },
+    base: mode === "production" ? "Portfolio" : "/",
+}));
