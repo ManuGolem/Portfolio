9af5ba27e774ec761dcdf431a8a91de935c75ec0
ManuGolem <manuelcarreras2013@gmail.com>
2025-07-17 19:32:09 -0300

    agrego ajedrez y quito toDo

--- src/Componnets/Page/Sections/Proyectos.jsx
+++ src/Componnets/Page/Sections/Proyectos.jsx
@@ -3,19 +3,11 @@ import { Proyecto } from "./Proyecto";
 export function Proyectos({ textProyectos }) {
     return (
         <section id="proyectos" className="bg-proyecto">
-            <h1 className="text-center mt-5 md:text-3xl text-2xl font-bold text-aqua">
-                {textProyectos.title}
-            </h1>
+            <h1 className="text-center mt-5 md:text-3xl text-2xl font-bold text-aqua">{textProyectos.title}</h1>
             <hr className="h-[4px] bg-naranja text-naranja rounded-2xl md:w-96 w-80 mx-auto"></hr>
             <div className="grid md:grid-cols-2 grid-cols-1 justify-items-center gap-10 md:p-10 py-10">
                 {textProyectos.proyectos.map((p) => (
-                    <Proyecto
-                        key={p.id}
-                        title={p.title}
-                        img={p.img}
-                        link={p.link}
-                        description={p.parrafo}
-                    />
+                    <Proyecto key={p.id} title={p.title} img={p.img} link={p.link} description={p.parrafo} />
                 ))}
             </div>
         </section>
